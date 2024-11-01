961e440006d079b8aa461c8ee3e4ab3f67bd409e
David <davvan@pdx.edu>
2024-11-01 13:35:19 -0700

    Render posts at index, added css, favicon

--- network/models.py
+++ network/models.py
@@ -6,7 +6,10 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     date = models.DateTimeField(auto_now_add=True)
-    # likes = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
     # comments_count = models.PositiveIntegerField
 
+class Like(models.Model):
+    liked_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='liked_posts', on_delete=models.CASCADE)
+    liked_post = models.ForeignKey(Post, related_name='liked_by', on_delete=models.CASCADE)
 
--- network/views.py
+++ network/views.py
@@ -2,12 +2,40 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from .models import Post
+from .models import Post, Like
 
 # Create your views here.
 def index(request):
-    return render(request, "network/index.html")
+    posts = Post.objects.all().order_by('-date')
+    posts, liked_posts = create_posts(request, posts)
+    return render(request, "network/index.html", {
+        'posts': posts,
+        'liked_posts': liked_posts
+    })
+
+def create_posts(request, posts):
+    """
+    Helper funcion to paginate posts and get liked posts by user
+    """
+    paginator = Paginator(posts, 10)
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    
+    if request.user.is_authenticated:
+        # get the ID's of posts that are liked on the current page
+        liked_posts = Like.objects.filter(liked_by= request.user, liked_post__in=posts).values_list('liked_post_id', flat=True)
+    else:
+        liked_posts = []
+    return posts, liked_posts
+
 
 @login_required
 def post(request):
